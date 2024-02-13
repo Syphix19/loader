17639ec54812101e3e9dc4ce46f3142c73bdc0ed
Syphix <128835035+Syphix19@users.noreply.github.com>
2024-02-13 20:46:26 +0400

    Update Loader main.lua

--- Loader main.lua
+++ Loader main.lua
@@ -2,7 +2,7 @@ local placeId = game.PlaceId
 
 
 if placeId == 5233782396 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Syphix19/Creatures-of-sonaria-syphix-keido-ui-main/main/New%20script", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Syphix19/Creatures-of-sonaria-syphix-keido-ui-main/main/Script", true))()
  elseif placeId == 11727501024 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Syphix19/Create-a-Cart-Ride/main/Script"))()
  elseif placeId == 12242070418 then
